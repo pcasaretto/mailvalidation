c7db4c78ec0aa0de45c4063c2b6c73916fcfc4eb
pcasaretto <paulo.casaretto@resultadosdigitais.com.br>
2016-05-09 00:18:06 -0300

    Refactor code and specs

--- validator_test.go
+++ validator_test.go
@@ -1,42 +1,125 @@
 package mailvalidation_test
 
 import (
-	"fmt"
+	"errors"
+	"net"
 	"net/mail"
 	"testing"
+	"time"
 
 	"github.com/pcasaretto/mailvalidation"
 )
 
-func TestDNSLookupValidatorValidate(t *testing.T) {
-}
-
 func TestValidate(t *testing.T) {
-	validator := mailvalidation.NewDNSLookupValidator(nil)
 
-	tests := []struct {
-		in  string
-		out bool
-	}{
-		{"dandrews0@delicious.com", true},
-		{"lwashington1@geocities.jp", false},
-		{"bthomas2@google.com.hk", true},
-		{"lnelson3@liveinternet.ru", false},
-		{"jhunter4@cnn.com", false},
-		{"jhamilton5@adobe.com", true},
-		{"calexander6@bloglines.com", false},
-		{"eolson7@amazonaws.com", false},
-		{"rmartinez8@businessinsider.com", false},
-		{"jlynch9@arizona.edu", false},
+	addr, _ := mail.ParseAddress("pcasaretto@gmail.com")
+	// mx := &net.MX{}
+
+	goodConnection := func(network, address string, timeout time.Duration) (net.Conn, error) {
+		conn, _ := net.Pipe()
+		return conn, nil
+	}
+	badConnection := func(network, address string, timeout time.Duration) (net.Conn, error) {
+		return nil, errors.New("error")
 	}
 
-	for _, m := range tests {
-		addr, _ := mail.ParseAddress(m.in)
+	tests := []struct {
+		Description string
+		LookupMX    func(host string) ([]*net.MX, error)
+		LookupIP    func(host string) ([]net.IP, error)
+		DialTimeout func(network, address string, timeout time.Duration) (net.Conn, error)
+		Expected    bool
+	}{
+		{
+			"All nils",
+			func(host string) (mxs []*net.MX, err error) {
+				return nil, nil
+			},
+			func(host string) (ips []net.IP, err error) {
+				return nil, nil
+			},
+			goodConnection,
+			false,
+		},
+		{
+			"MX ok",
+			func(host string) (mxs []*net.MX, err error) {
+				return []*net.MX{&net.MX{}}, nil
+			},
+			func(host string) (ips []net.IP, err error) {
+				return nil, nil
+			},
+			goodConnection,
+			true,
+		},
+		{
+			"MX ok, bad connection",
+			func(host string) (mxs []*net.MX, err error) {
+				return []*net.MX{&net.MX{}, &net.MX{}}, nil
+			},
+			func(host string) (ips []net.IP, err error) {
+				return nil, nil
+			},
+			badConnection,
+			false,
+		},
+		{
+			"MX error and IP ok",
+			func(host string) (mxs []*net.MX, err error) {
+				return nil, errors.New("error")
+			},
+			func(host string) (ips []net.IP, err error) {
+				return []net.IP{net.IP{}, net.IP{}}, nil
+			},
+			goodConnection,
+			true,
+		},
+		{
+			"MX error and IP ok, bad connection",
+			func(host string) (mxs []*net.MX, err error) {
+				return nil, errors.New("error")
+			},
+			func(host string) (ips []net.IP, err error) {
+				return []net.IP{net.IP{}}, nil
+			},
+			badConnection,
+			false,
+		},
+	}
+
+	for i, test := range tests {
+
+		mailvalidation.LookupMX = test.LookupMX
+		mailvalidation.LookupIP = test.LookupIP
+		mailvalidation.DialTimeout = test.DialTimeout
+		validator := mailvalidation.NewDNSLookupValidator()
 		valid := validator.Validate(addr)
 
-		if valid != m.out {
-			fmt.Println(addr)
-			t.Errorf("got %t, want %t", valid, m.out)
+		if valid != test.Expected {
+			t.Errorf("test %d:\n\rgot %t\n\rwant %t", i, valid, test.Expected)
 		}
+	}
+}
+
+func TestValidateTimeout(t *testing.T) {
+	mailvalidation.LookupMX = func(host string) (mxs []*net.MX, err error) {
+		return []*net.MX{&net.MX{}}, nil
+	}
+	mailvalidation.LookupIP = func(host string) (ips []net.IP, err error) {
+		return nil, nil
+	}
+	mailvalidation.DialTimeout = func(network, address string, timeout time.Duration) (net.Conn, error) {
+		time.Sleep(100 * time.Nanosecond)
+		return nil, nil
+	}
+
+	validator := mailvalidation.NewDNSLookupValidator()
+	validator.Timeout = time.Nanosecond
+
+	addr, _ := mail.ParseAddress("pcasaretto@gmail.com")
+
+	valid := validator.Validate(addr)
+	if valid {
+		t.Errorf("got %t\n\rwant %t", valid, false)
 	}
 }
